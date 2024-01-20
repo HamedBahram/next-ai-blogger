c75c8c6b1ee11ef5918e667c8782ffbe7a26870c
Hamed Bahram <hamed.bahram@gmail.com>
2024-01-20 15:05:24 -0500

    Update dynamic blog page

--- app/blog/[id]/page.tsx
+++ app/blog/[id]/page.tsx
@@ -8,19 +8,21 @@ export default async function Blog({ params }: { params: { id: string } }) {
   const { content, imageUrl } = await getBlogById(Number(params.id))
 
   return (
-    <section className='mx-auto max-w-xl py-12'>
-      <Link
-        href='/'
-        className='-ml-2 inline-flex items-center text-sm font-light text-gray-500 no-underline hover:text-gray-700'
-      >
-        <ChevronLeft strokeWidth={1} size={20} />
-        <span>Go back</span>
-      </Link>
+    <section className='py-12'>
+      <div className='container max-w-3xl'>
+        <Link
+          href='/'
+          className='-ml-2 inline-flex items-center text-sm font-light text-gray-500 no-underline hover:text-gray-700'
+        >
+          <ChevronLeft strokeWidth={1} size={20} />
+          <span>Go back</span>
+        </Link>
 
-      <section className='prose mt-6'>
-        <Image alt='' src={imageUrl} width={1792} height={1024} />
-        <Markdown>{content}</Markdown>
-      </section>
+        <section className='prose mt-6 max-w-none'>
+          <Image alt='' src={imageUrl} width={1792} height={1024} />
+          <Markdown>{content}</Markdown>
+        </section>
+      </div>
     </section>
   )
 }
